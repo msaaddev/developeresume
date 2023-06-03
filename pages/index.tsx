9b9d05522881fbbe8f22953b54e005738650dbea
msaaddev <mrsaadirfan@gmail.com>
2023-06-03 23:00:10 +0200

    📦 NEW: Show section if data exists

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
 import { Header, Education, Skills, Experience, Projects, Accomplishments } from '@/components';
 import Head from 'next/head';
-import data from '@/data'
+import data from '@/data';
 
 export default function Home() {
+    const title = `resume-${data.header.name.toLowerCase().split(' ').join('-')}`;
+
     return (
         <>
             <Head>
-                <title>resume-muhammad-saad</title>
+                <title>{title}</title>
             </Head>
             <div className='bg-white min-h-screen text-black space-y-3 font-light'>
                 <Header />
-                <Experience />
-                <Education />
-                <Projects />
-                <Skills />
-                <Accomplishments />
+                {data.experience.length ? <Experience /> : null}
+                {data.education.length ? <Education /> : null}
+                {data.projects.length ? <Projects /> : null}
+                {data.skills.length ? <Skills /> : null}
+                {data.accomplishments.length ? <Accomplishments /> : null}
             </div>
         </>
     );
